660b47359e3667d689cf3df0cacfcdc24beee048
goatz92 <goataragua@gmail.com>
2025-10-07 14:22:56 +0300

    finalize main

--- falling-sand-simulation/main.py
+++ falling-sand-simulation/main.py
@@ -1,18 +1,35 @@
 import pygame
+import simulation import Simulation
 
 pygame.init()
+pygame.mouse.set_visible(False)
 
-# Pygame 
+# Pygame constants
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 CELL_SIZE = 6
 FPS = 120
 GREY = (29, 29, 29)
 
+# Pygame display
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Falling Sand")
 
 clock = pygame.time.Clock()
-# simulation = Simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
+simulation = Simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
 
-#
+# Simulation loop
+while True:
+    
+    # 1. Event Handling
+    simulation.handle_controls()
+
+    # 2. Update State
+    simulation.update()
+
+    # 3. Draw
+    window.fill(GREY)
+    simulation.draw(window)
+
+    pygame.display.flip()
+    clock.tick(FPS)
